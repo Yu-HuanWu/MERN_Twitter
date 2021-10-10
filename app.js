f2a9546d943eacc4be1b36fe625032a8328cf48a
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-10-09 21:59:00 -0700

    finished login route setup

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const mongoose = require("mongoose")
 const db= require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
+// const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(()=> console.log("Connected to mongoDB"))
     .catch(err => console.log(err))
 
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get("/", (req, res)=> {
+    const user = new User({
+        handle: "jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    })
+    user.save()
     res.send("Hello World!");
 });
 
