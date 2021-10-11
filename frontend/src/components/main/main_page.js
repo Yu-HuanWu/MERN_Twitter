d75790448218f286ead69799f80400e5a8efc161
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-10-10 18:28:19 -0700

    finished sesion_reducer

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>A Twitter Clone</h1>
+                <footer>
+                    Copyright &copy; 2021 Tw@%#er
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
